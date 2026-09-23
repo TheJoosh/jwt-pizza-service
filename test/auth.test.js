c48e468a4783a785287e99153f2194151db715db
Josh <joshbbrown1007@gmail.com>
2026-09-23 16:35:26 -0600

    feat: add CI pipeline, testing, and linting setup
    - Updated package.json to include test, coverage, and lint scripts.
    - Added devDependencies for ESLint and Jest.
    - Created a CI workflow in GitHub Actions for automated testing and linting.
    - Configured ESLint with recommended settings and globals.
    - Added Jest configuration for coverage reporting.
    - Implemented initial test suite for authentication using Supertest.

---
+++ test/auth.test.js
@@ -0,0 +1,36 @@
+const request = require('supertest');
+const app = require('../src/service');
+//const { DB } = require('../src/database/database.js');
+
+const testUser = { name: 'pizza diner', email: 'reg@test.com', password: 'a' };
+let testUserAuthToken;
+
+function randomName() {
+  return Math.random().toString(36).substring(2, 12);
+}
+
+// async function createAdminUser() {
+//   let user = { password: 'toomanysecrets', roles: [{ role: Role.Admin }] };
+//   user.name = randomName();
+//   user.email = user.name + '@admin.com';
+
+//   await DB.addUser(user);
+//   user.password = 'toomanysecrets';
+
+//   return user;
+// }
+
+beforeAll(async () => {
+  testUser.email = randomName() + '@test.com';
+  const registerRes = await request(app).post('/api/auth').send(testUser);
+  testUserAuthToken = registerRes.body.token;
+});
+
+test('login', async () => {
+  const loginRes = await request(app).put('/api/auth').send(testUser);
+  expect(loginRes.status).toBe(200);
+  expect(loginRes.body.token).toMatch(/^[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*$/);
+
+  const { password, ...user } = { ...testUser, roles: [{ role: 'diner' }] };
+  expect(loginRes.body.user).toMatchObject(user);
+});
